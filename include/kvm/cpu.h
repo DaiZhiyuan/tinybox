68017e9a172c96484e978c5a1dca664ad242a677
Zhiyuan Dai <jerry.zhiyuan.dai@gmail.com>
2021-11-17 03:00:40 -0500

    Clean up KVM initialization
    Signed-off-by: Zhiyuan Dai <jerry.zhiyuan.dai@gmail.com>

--- include/kvm/cpu.h
+++ include/kvm/cpu.h
@@ -23,16 +23,16 @@ enum eflag_bits {
 };
 
 struct cpu_registers {
-    uint32_t    eax;
-    uint32_t    ebx;
-    uint32_t    ecx;
-    uint32_t    edx;
-    uint32_t    esp;
-    uint32_t    ebp;
-    uint32_t    esi;
-    uint32_t    edi;
-    uint32_t    eip;
-    uint32_t    eflags;
+    uint32_t eax;
+    uint32_t ebx;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t esp;
+    uint32_t ebp;
+    uint32_t esi;
+    uint32_t edi;
+    uint32_t eip;
+    uint32_t eflags;
 };
 
 struct cpu {
