8b4a3549bd5b7f20bd0a5d5b0bce378612568ef7
Zhiyuan Dai <daizhiyuan@phytium.com.cn>
2021-11-18 01:52:36 -0500

    Setup rflags properly to fix EXIT_UNKNOWN
    Signed-off-by: Zhiyuan Dai <daizhiyuan@phytium.com.cn>

--- kvm.c
+++ kvm.c
@@ -152,13 +152,13 @@ static void kvm__show_registers(struct kvm *self)
     unsigned long rbp,  r8,  r9;
     unsigned long r10, r11, r12;
     unsigned long r13, r14, r15;
-    unsigned long rip, rsp;
+    unsigned long rflags, rip, rsp;
     struct kvm_regs regs;
 
     if (ioctl(self->vcpu_fd, KVM_GET_REGS, &regs) < 0)
         die("KVM_GET_REGS failed");
 
-    rip = regs.rip; rsp = regs.rsp;
+    rflags = regs.rflags; rip = regs.rip; rsp = regs.rsp;
     rax = regs.rax; rbx = regs.rbx; rcx = regs.rcx;
     rdx = regs.rdx; rsi = regs.rsi; rdi = regs.rdi;
     rbp = regs.rbp; r8  = regs.r8;  r9  = regs.r9;
@@ -166,12 +166,12 @@ static void kvm__show_registers(struct kvm *self)
     r13 = regs.r13; r14 = regs.r14; r15 = regs.r15;
 
     printf("Registers:\n");
-    printf(" rip: %016lx   rsp: %016lx\n", rip, rsp);
-    printf(" rax: %016lx   ebx: %016lx   ecx: %016lx\n", rax, rbx, rcx);
-    printf(" rdx: %016lx   rsi: %016lx   rdi: %016lx\n", rdx, rsi, rdi);
-    printf(" rbp: %016lx   r8:  %016lx   r9:  %016lx\n", rbp, r8,  r9);
-    printf(" r10: %016lx   r11: %016lx   r12: %016lx\n", r10, r11, r12);
-    printf(" r13: %016lx   r14: %016lx   r15: %016lx\n", r13, r14, r15);
+    printf("rflags: %016lx   rip: %016lx   rsp: %016lx\n", rip, rsp);
+    printf("   rax: %016lx   ebx: %016lx   ecx: %016lx\n", rax, rbx, rcx);
+    printf("   rdx: %016lx   rsi: %016lx   rdi: %016lx\n", rdx, rsi, rdi);
+    printf("   rbp: %016lx    r8: %016lx    r9: %016lx\n", rbp, r8,  r9);
+    printf("   r10: %016lx   r11: %016lx   r12: %016lx\n", r10, r11, r12);
+    printf("   r13: %016lx   r14: %016lx   r15: %016lx\n", r13, r14, r15);
 }
 
 static inline void *guest_addr_to_host(struct kvm *self, unsigned long offset)
@@ -269,6 +269,12 @@ static const char *exit_reasons[] = {
     DEFINE_KVM_EXIT_REASON(KVM_EXIT_INTERNAL_ERROR),
 };
 
+static void kvm__reset_vcpu(struct kvm *self, uint64_t rip)
+{
+    self->regs.rip = rip;
+    self->regs.rflags = 0x0000000000000002ULL;
+}
+
 static void usage(char *argv[])
 {
     fprintf(stderr, "  usage: %s <kernel-image>\n", argv[0]);
@@ -278,6 +284,7 @@ static void usage(char *argv[])
 int main(int argc, char *argv[])
 {
     const char *kernel_filename;
+    uint64_t kernel_start;
     struct kvm *kvm;
     int ret;
 
@@ -288,7 +295,9 @@ int main(int argc, char *argv[])
     
     kvm = kvm__init();
 
-    kvm->regs.rip = kvm__load_kernel(kvm, kernel_filename);
+    kernel_start = kvm__load_kernel(kvm, kernel_filename);
+
+    kvm__reset_vcpu(kvm, kernel_start);
 
     kvm__run(kvm);
 
