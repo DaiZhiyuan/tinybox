b90ab4cea56d39ddd0066f063bf37c785800d05f
Zhiyuan Dai <jerry.zhiyuan.dai@gmail.com>
2021-11-28 20:52:46 -0500

    kvm: Ignore CMOS RAM/RTC ioports
    Signed-off-by: Zhiyuan Dai <jerry.zhiyuan.dai@gmail.com>

--- ioport.c
+++ ioport.c
@@ -50,21 +50,25 @@ static struct ioport_operations dummy_write_only_ioport_ops = {
 };
 
 static struct ioport_operations *ioport_ops[USHRT_MAX] = {
-    /* 0x0070 - 0x007F - CMOS RAM/RTC (REAL TIME CLOCK) */
+    /* PORT 0x0070 - 0x007F - CMOS RAM/RTC (REAL TIME CLOCK) */
     [0x70] = &cmos_ram_rtc_ops,
     [0x71] = &dummy_read_write_ioport_ops,
 
-    /* 0x0020 - 0x003F - 8259A PIC 1*/
+    /* PORT 0x0020 - 0x003F - 8259A PIC 1*/
     [0x20] = &dummy_read_write_ioport_ops,
     [0x21] = &dummy_read_write_ioport_ops,
 
-    /* 0x00A0) - 0x00AF - 8259A PIC 2*/
+    /* PORT 0x00A0 - 0x00AF - 8259A PIC 2*/
     [0xA0] = &dummy_read_write_ioport_ops,
     [0xA1] = &dummy_read_write_ioport_ops,
 
-    /* 0xf00f0 - 0x00ff - Math co-processor */
+    /* PORT 0xf00f0 - 0x00ff - Math co-processor */
     [0xF0] = &dummy_write_only_ioport_ops,
     [0xF1] = &dummy_write_only_ioport_ops,
+
+    /* PORT 03D4-03D5 - COLOR VIDEO - CRT CONTROL REGISTERS */
+    [0x3D4] = &dummy_read_write_ioport_ops,
+    [0x3D5] = &dummy_write_only_ioport_ops,
 
     /* PORT 0CF8-0CFF - PCI Configuration Mechanism 1 - Configuration Registers */
     [0x0CF8] = &dummy_write_only_ioport_ops,
