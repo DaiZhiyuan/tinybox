b7d49704763ac0f6bd61c955466336c3fc5d5145
Zhiyuan Dai <jerry.zhiyuan.dai@gmail.com>
2021-11-29 23:27:06 -0500

    kvm: Check for read() return value in load_bzimage()
    Signed-off-by: Zhiyuan Dai <jerry.zhiyuan.dai@gmail.com>

--- kvm.c
+++ kvm.c
@@ -294,7 +294,8 @@ static bool load_bzimage(struct kvm *self, int fd, const char *kernel_cmdline)
      * memory layout.
      */
 
-    read(fd, &boot, sizeof(boot));
+    if (read(fd, &boot, sizeof(boot)) != sizeof(boot))
+        die_perror("read");
     
     if (memcmp(&boot.hdr.header, BZIMAGE_MAGIC, strlen(BZIMAGE_MAGIC)) != 0)
         return false;
