aaca905195831818e2a33b52bf457de7819fcde4
Zhiyuan Dai <jerry.zhiyuan.dai@gmail.com>
2021-11-30 02:26:01 -0500

    kvm, pci: Add dummy virtio device for probing
    Signed-off-by: Zhiyuan Dai <jerry.zhiyuan.dai@gmail.com>

--- include/kvm/pci.h
+++ include/kvm/pci.h
@@ -1,6 +1,9 @@
 #ifndef KVM__PCI_H
 #define KVM__PCI_H
 
+#include <inttypes.h>
+#include "pci_regs.h"
+
 /* some known offsets and register names */
 #define PCI_CONFIG_ADDRESS  0xcf8
 #define PCI_CONFIG_DATA     0xcfc
@@ -15,6 +18,31 @@ struct pci_config_address {
     unsigned                bus_number      : 8;            /* 23 .. 16 */
     unsigned                reserved        : 7;            /* 30 .. 24 */
     unsigned                enable_bit      : 1;            /* 31       */
+};
+
+struct pci_device_header {
+    uint16_t        vendor_id;
+    uint16_t        device_id;
+    uint16_t        command;
+    uint16_t        status;
+    uint16_t        revision_id             :  8;
+    uint32_t        class                   : 24;
+    uint8_t         cacheline_size;
+    uint8_t         latency_timer;
+    uint8_t         header_type;
+    uint8_t         BIST;
+    uint32_t        BAR[6];
+    uint32_t        card_bus;
+    uint16_t        subsys_vendor_id;
+    uint16_t        subsys_id;
+    uint32_t        exp_rom_bar;
+    uint32_t        capabilities            :  8;
+    uint32_t        reserved1               : 24;
+    uint32_t        reserved2;
+    uint8_t         irq_line;
+    uint8_t         irq_pin;
+    uint8_t         min_gnt;
+    uint8_t         max_lat;
 };
 
 void pci__init(void);
