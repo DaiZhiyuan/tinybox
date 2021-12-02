e05d2815d54568d539fd171eeeddbb9a4ab6b791
Zhiyuan Dai <jerry.zhiyuan.dai@gmail.com>
2021-12-01 20:17:30 -0500

    kvm: Fail virtio blk ioports by default
    This patch changes virtio block device ioports to fail by default and
    adds few specific stubs to keep kernel booting still.
    
    Signed-off-by: Zhiyuan Dai <jerry.zhiyuan.dai@gmail.com>

--- blk-virtio.c
+++ blk-virtio.c
@@ -4,7 +4,75 @@
 #include "kvm/ioport.h"
 #include "kvm/pci.h"
 
+#include <stdio.h>
+
 #define VIRTIO_PCI_IOPORT_SIZE      24
+
+struct device {
+    uint32_t guest_features;
+    uint8_t status;
+};
+
+static struct device device;
+
+static bool blk_virtio_in(struct kvm *self, uint16_t port, void *data, int size, uint32_t count)
+{
+    unsigned long offset;
+
+    offset = port - IOPORT_VIRTIO;
+
+    switch (offset) {
+    case VIRTIO_PCI_HOST_FEATURES:
+        ioport__write32(data, 0x00);
+        break;
+    case VIRTIO_PCI_GUEST_FEATURES:
+    case VIRTIO_PCI_QUEUE_PFN:
+    case VIRTIO_PCI_QUEUE_NUM:
+    case VIRTIO_PCI_QUEUE_SEL:
+    case VIRTIO_PCI_QUEUE_NOTIFY:
+        return false;
+    case VIRTIO_PCI_STATUS:
+        ioport__write8(data, device.status);
+        break;
+    case VIRTIO_PCI_ISR:
+    case VIRTIO_MSI_CONFIG_VECTOR:
+    default:
+        return false;
+    };
+
+    return true;
+}
+
+static bool blk_virtio_out(struct kvm *self, uint16_t port, void *data, int size, uint32_t count)
+{
+    unsigned long offset;
+
+    offset = port - IOPORT_VIRTIO;
+
+    switch (offset) {
+    case VIRTIO_PCI_GUEST_FEATURES:
+        device.guest_features = ioport__read32(data);
+        break;
+    case VIRTIO_PCI_QUEUE_PFN:
+    case VIRTIO_PCI_QUEUE_SEL:
+    case VIRTIO_PCI_QUEUE_NOTIFY:
+        return false;
+    case VIRTIO_PCI_STATUS:
+        device.status = ioport__read8(data);
+        break;
+    case VIRTIO_MSI_CONFIG_VECTOR:
+    case VIRTIO_MSI_QUEUE_VECTOR:
+    default:
+        return false;
+    };
+
+    return true;
+}
+
+static struct ioport_operations blk_virtio_io_ops = {
+    .io_in = blk_virtio_in,
+    .io_out = blk_virtio_out,
+};
 
 #define PCI_VENDOR_ID_REDHAT_QUMRANET           0x1af4
 #define PCI_DEVICE_ID_VIRTIO_BLK                0x1001
@@ -20,53 +88,6 @@ static struct pci_device_header blk_virtio_device = {
     .subsys_vendor_id       = PCI_SUBSYSTEM_VENDOR_ID_REDHAT_QUMRANET,
     .subsys_id              = PCI_SUBSYSTEM_ID_VIRTIO_BLK,
     .bar[0]                 = IOPORT_VIRTIO | PCI_BASE_ADDRESS_SPACE_IO,
-};
-
-static bool blk_virtio_in(struct kvm *self, uint16_t port, void *data, int size, uint32_t count)
-{
-    unsigned long offset;
-
-    offset = port - IOPORT_VIRTIO;
-
-    switch (offset) {
-    case VIRTIO_PCI_HOST_FEATURES:
-    case VIRTIO_PCI_GUEST_FEATURES:
-    case VIRTIO_PCI_QUEUE_PFN:
-    case VIRTIO_PCI_QUEUE_NUM:
-    case VIRTIO_PCI_QUEUE_SEL:
-    case VIRTIO_PCI_QUEUE_NOTIFY:
-    case VIRTIO_PCI_STATUS:
-    case VIRTIO_PCI_ISR:
-    case VIRTIO_MSI_CONFIG_VECTOR:
-        return true;
-    };
-
-    return false;
-}
-
-static bool blk_virtio_out(struct kvm *self, uint16_t port, void *data, int size, uint32_t count)
-{
-    unsigned long offset;
-
-    offset = port - IOPORT_VIRTIO;
-
-    switch (offset) {
-    case VIRTIO_PCI_GUEST_FEATURES:
-    case VIRTIO_PCI_QUEUE_PFN:
-    case VIRTIO_PCI_QUEUE_SEL:
-    case VIRTIO_PCI_QUEUE_NOTIFY:
-    case VIRTIO_PCI_STATUS:
-    case VIRTIO_MSI_CONFIG_VECTOR:
-    case VIRTIO_MSI_QUEUE_VECTOR:
-        return true;
-    };
-
-    return false;
-}
-
-static struct ioport_operations blk_virtio_io_ops = {
-    .io_in = blk_virtio_in,
-    .io_out = blk_virtio_out,
 };
 
 void blk_virtio__init(void)
