74c8cbe1a35db016077e198cc7f26a5159714714
Daizhiyuan <jerry.zhiyuan.dai@gmail.com>
2021-12-06 16:30:13 +0800

    kvm,8250: Fix ->counter clearing
    We need to reset the counter to zero before we inject an interrupt to
    the guest.
    
    Signed-off-by: Daizhiyuan <jerry.zhiyuan.dai@gmail.com>

--- 8250-serial.c
+++ 8250-serial.c
@@ -103,6 +103,7 @@ void serial8250__interrupt(struct kvm *self)
 
     /* Only send an IRQ if there's work to do. */
     if (new_iir) {
+        dev->counter = 0;
         dev->iir = new_iir;
         kvm__irq_line(self, dev->irq, 0);
         kvm__irq_line(self, dev->irq, 1);
@@ -163,10 +164,8 @@ static bool serial8250_out(struct kvm *self, uint16_t port, void *data, int size
             }
             fflush(stdout);
 
-            if (dev->counter++ > 10) {
+            if (dev->counter++ > 10)
                 dev->iir = UART_IIR_NO_INT;
-                dev->counter = 0;
-            }
 
             break;
         }
